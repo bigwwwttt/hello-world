1a55e25328407aeafc46ca0222dc03297235b251
bigwwwttt <151817548+bigwwwttt@users.noreply.github.com>
2023-11-23 20:17:01 +0800

    Update fifo_tb.scala

--- fifo_tb.scala
+++ fifo_tb.scala
@@ -1,3 +1,38 @@
 import chisel3._
-import chiseltest._
+import chiseltester._
 
+
+test(new FIFO(UInt(8.W), 8)){c =>
+    //test write
+    c.io.out.ready.poke(false.B)
+    c.io.in.valid.poke(true.B)
+    for(i <- 0 until 8){
+        val datain = i * i + 1
+        c.io.in.bits.poke((datain.U))//write data
+        c.clock.step(1)
+    }
+    c.io.full.expect(1.B)//test full, expect full = 1
+    println("write test success")
+
+    //test read
+    c.io.out.ready.poke(true.B)
+    c.io.in.valid.poke(false.B)
+    for(i <- 0 until 8){
+        val datain = i * i + 1
+        c.io.out.bits.expect((datain.U))//read data
+        c.clock.step(1)
+    }
+    c.io.empty.expect(1.B)//test empty, expect empty = 1
+    println("read test success")
+
+    //test Simultaneous read and write
+    c.io.out.ready.poke(true.B)
+    c.io.in.valid.poke(true.B)
+    for(i <- 0 until 8){
+        val datain = i * i + 1
+        c.io.in.bits.poke((datain.U))
+        c.clock.step(1)
+        c.io.out.bits.expect((datain.U))
+    }
+    println("write and read test success")
+}
