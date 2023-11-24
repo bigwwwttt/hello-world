f4d3407161130d39b0b0a2e0dd31142cbc72b650
bigwwwttt <151817548+bigwwwttt@users.noreply.github.com>
2023-11-24 19:03:53 +0800

    Update syncFIFO.scala

--- syncFIFO.scala
+++ syncFIFO.scala
@@ -7,7 +7,14 @@ class FIFO[T <: Data](ioType: T, depth: Int) extends Module {
     val out = Decoupled(ioType)
     val full = Output(Bool())
     val empty = Output(Bool())
+    val widx = Output(UInt((log2Ceil(depth)).W))
+    val ridx = Output(UInt((log2Ceil(depth)).W))
   })
+  require(depth>0 && isPow2(depth))
+
+  io.widx := Counter(io.in.valid && !io.full, depth)._1
+  io.ridx := Counter(io.out.ready && !io.empty, depth)._1
+
   io.out <> Queue(io.in, depth)
   io.full := !io.in.ready
   io.empty := !io.out.valid
